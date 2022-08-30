e081c1dbc43f720111ec5018b4d515c3799a142d
Ashar <Asharjaved2015@gmail.com>
2022-08-30 18:53:53 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import Banner from './components/banner/banner.component.jsx'
+import DisplayIP, { IP_Types } from './components/display-ip/display-ip.component';
 import Exhibit from './components/exhibit/exhibit.component';
 
 function App() {
   return (
     <div className="App">
-      <Banner title={"Site Title"}/>
-      <Exhibit title={"Exhibit Title"}>
+      <Banner title={"Sextent"}/>
+      <Exhibit title={"Your Public IPs are: "}>
         <div style={{display: "grid"}}>
-          <span>This is a test</span>
-          <span>This is a test</span>
-          <span>This is a test</span>
-          <span>This is a test</span>
+          <DisplayIP type={IP_Types.IPV4}/>
+          <DisplayIP type={IP_Types.IPV6}/>
         </div>
       </Exhibit>
     </div>
