819b1700417e64446d706c309871edb3bde46f12
Ashar <Asharjaved2015@gmail.com>
2022-09-02 12:44:26 +0100

    ignore this commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+//import Banner from './components/banner/banner.component.jsx'
+import DisplayIP, { IP_Types } from './components/display-ip/display-ip.component';
+//import Exhibit from './components/exhibit/exhibit.component';
+import PaylonLatency from './components/pylon-latency/pylon-latency.component';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <div style={{display: "grid"}}>
+          <DisplayIP type={IP_Types.IPV4}/>
+          <DisplayIP type={IP_Types.IPV6}/>
+          <PaylonLatency/>
+        </div>
+     
+    </div>
+  );
+}
+
+export default App;
